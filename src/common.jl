e8387f8c0ffaf32da3b24513170965834bf841d4
lukketotte <lukas.arnroth@gmail.com>
2025-06-19 15:41:21 +0200

    not sure

--- src/common.jl
+++ src/common.jl
@@ -1,4 +1,4 @@
-const var_dists = Dict{Symbol, Distribution}()
+const var_dists = Dict{Symbol,Distribution}()
 # @variables_with_dist x ~ Normal(0, 1)
 # var_dists[:x] gives Normal(0,1)
 macro variables_with_dist(expr)
@@ -6,19 +6,19 @@ macro variables_with_dist(expr)
     if !(expr isa Expr && expr.head == :call && expr.args[1] === :~)
         error("@variables_with_dist expects syntax like `x ~ Distribution(...)`")
     end
-    
+
     var_sym = expr.args[2]
     dist_expr = expr.args[3]
-    
+
     # Generate the variable as a Symbolics.Num
     var_def = :(Symbolics.@variables $var_sym)
-    
+
     # Evaluate the distribution expression to get the Distribution object
     dist_val = eval(dist_expr)
-    
+
     # Store mapping in var_dists dictionary
     push!(var_dists, var_sym => dist_val)
-    
+
     # Return the variable symbol for use
     return var_def
 end
